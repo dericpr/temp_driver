813c1aa811b34cc958157274107897663feb452c
Deric Panet-Raymond <=>
2015-07-15 11:30:22 -0700

    Code cleanup and refactor

--- main.c
+++ main.c
@@ -24,12 +24,16 @@
 #include "crankbrew.h"
 #include <fcntl.h>
 #include <dirent.h>
+#include <syslog.h>
 
 char * temp_wort;
 char * temp_ambient;
 char * web_address;
-
-float setpoint = 13.0f;
+char *heat_control = "/sys/class/gpio/gpio51/value";
+char *cool_control = "/sys/class/gpio/gpio60/value";
+int cool_fd;
+int heat_fd;
+float setpoint = 16.0f;
 
 struct MemoryStruct {
 	  char *memory;
@@ -45,7 +49,7 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 	  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
 	  if(mem->memory == NULL) {
 		  /* out of memory! */ 
-		  printf("not enough memory (realloc returned NULL)\n");
+		  syslog(LOG_ERR,"not enough memory (realloc returned NULL)\n");
 		  return 0;
 	  }
 							    
@@ -103,7 +107,6 @@ int temp_update(gre_io_t *send_handle, char * target, temp_update_t *event_data)
 	gre_io_serialized_data_t    *nbuffer = NULL;
 	int ret;
 
-		printf("%s : %d\n", __FUNCTION__,__LINE__);
 	/*
 	 * Send a named event with an additional structure payload
 	 */
@@ -114,13 +117,56 @@ int temp_update(gre_io_t *send_handle, char * target, temp_update_t *event_data)
 				 sizeof(temp_update_t));
 
 	ret = gre_io_send(send_handle, nbuffer);
-	printf("Ret value : %d\n", ret);
 	//Free the allocated memory buffer for the serialized data
     gre_io_free_buffer(nbuffer);
     
 	return ret;
 }
 
+int control_temp(char *heat,char *chill)
+{
+	int ret;
+	int ret_1;
+    ret = write(heat_fd,heat,1);
+	ret_1 = write(cool_fd,chill,1);
+	return ret && ret_1;
+}
+
+float read_temp(int fp) 
+{
+	char 			tmpData[6];
+	unsigned char 	onewire[256];
+	float 			rounded_down;
+	int				num_read = 0;
+	int				failed_read = 0;	
+	float 			float_val = 0.0;
+
+	memset(onewire,0,256);
+	memset(tmpData,0,6);	
+
+	while((num_read = read(fp, onewire, 256)) > 0) 
+	{
+		if ( strstr(onewire, "NO") == NULL ) {
+			strncpy(tmpData, strstr(onewire, "t=") + 2, 5); 
+			float_val = strtof(tmpData, NULL);
+			float_val = float_val /1000;
+			syslog(LOG_INFO,"Wort : Temp: %.3f C\n", float_val);
+			failed_read = 0;
+		} else {
+			failed_read = 1;
+			break;
+		}
+	}
+	if ( failed_read == 1 ) {
+		syslog(LOG_ERR, "Failed to read Wort temp sensor, skipping to next cycle");
+		return -1;
+	}
+	rounded_down = ceilf(float_val * 100) / 100;
+	if (rounded_down > 100.0f) {
+		return -1;
+	}
+	return rounded_down;
+}
 
 static void * check_temperature(void * data) 
 {
@@ -129,150 +175,74 @@ static void * check_temperature(void * data)
 	int 			ret;
 	float 			old_ambient,old_wort;
 	int 			fp, fp1;
-	unsigned char 	onewire[256];
 	int 			a,b;
 	char 			hex_string[6];
 	int 			dec_val = 0;
-	float 			float_val = 0.0;
-	int				num_read = 0;
-	char 			tmpData[6];
-	int				failed_read = 0;
-
-	while (1) {
-		fp = open(temp_wort, O_RDONLY);
-	    fp1 = open(temp_ambient, O_RDONLY);
-		if ( fp != -1 && fp1 != -1 )
+	float 			temp_read;
+	fp = open(temp_wort, O_RDONLY);
+	fp1 = open(temp_ambient, O_RDONLY);
+	if ( fp != -1 && fp1 != -1 )
+	{
+		while (1) 
 		{
 			old_ambient = event_data.temp1;
 			old_wort = event_data.temp2;
-			//fgets(onewire, 6, fp);
-			//sscanf(&onewire[0], "%2x", &a);
-			//sscanf(&onewire[2], "%2x", &b);
-			//snprintf(hex_string, 6, "%02x%02x", b,a);
-			//sscanf(hex_string, "%x", &dec_val);
-			//float_val = ((float)dec_val * 62.5) / 1000;
-			while((num_read = read(fp, onewire, 256)) > 0) 
-  			{
-				if ( strstr(onewire, "NO") == NULL ) {
-   					strncpy(tmpData, strstr(onewire, "t=") + 2, 5); 
-   					float_val = strtof(tmpData, NULL);
-   					float_val = float_val /1000;
-					printf("Wort : Temp: %.3f C\n", float_val);
-					failed_read = 0;
-				} else {
-					failed_read = 1;
-					break;
-				}
-  			}
-			if ( failed_read == 1 ) {
-				close(fp);
-				close(fp1);
-				printf("SKIPPING FAILED READ\n");
-				continue;
+			
+			// read wort then read ambient
+			temp_read = read_temp(fp);
+			if ( temp_read ) {
+				event_data.temp1 = temp_read;
 			}
-			float rounded_down = ceilf(float_val * 100) / 100;
-			if (rounded_down > 100.0f) {
-				close(fp);
-				close(fp1);
-				continue;
+			temp_read = read_temp(fp1);
+			if ( temp_read ) {
+				event_data.temp2 = temp_read;
+				thread_data->good_read = 1;
 			}
-
-			event_data.temp1 = rounded_down;
-			memset(onewire,0,256);
-			memset(tmpData,0,6);	
-			while((num_read = read(fp1, onewire, 256)) > 0) 
-  			{
-				if ( strstr(onewire, "NO") == NULL ) {
-   					strncpy(tmpData, strstr(onewire, "t=") + 2, 5); 
-   					float_val = strtof(tmpData, NULL);
-					float_val = float_val / 1000;
-   					printf("Ambient Temp: %.3f C\n", float_val);
-					failed_read = 0;
-  				} else {
-					failed_read = 1;
-					break;
-				}
-			}
-			if ( failed_read == 1 ) {
-				close(fp);
-				close(fp1);
-				printf("SKIPPING FAILED READ\n");
-				continue;
-			}
-			//fgets(onewire, 6, fp1);
-			//sscanf(&onewire[0], "%2x", &a);
-			//sscanf(&onewire[2], "%2x", &b);
-			//snprintf(hex_string, 6, "%02x%02x", b,a);
-			//sscanf(hex_string, "%x", &dec_val);
-			//float_val = ((float)dec_val * 62.5) / 1000;
-			rounded_down = 0.0f;
-			rounded_down = ceilf(float_val * 100) / 100;
-			if (rounded_down > 100.0f) {
-				close(fp);
-				close(fp1);
-				continue;
-			}
-			event_data.temp2 = rounded_down;
-			thread_data->good_read = 1;
 			if ( event_data.temp1 != old_ambient || event_data.temp2 != old_wort ) {
 				thread_data->temp_data.temp1 = event_data.temp1;
 				thread_data->temp_data.temp2 = event_data.temp2;
-				printf("Got temp data of : %f:%f\n", thread_data->temp_data.temp1,thread_data->temp_data.temp2);
+				syslog(LOG_NOTICE,"Got temp data of : %f:%f\n", thread_data->temp_data.temp1,thread_data->temp_data.temp2);
 			//	ret = temp_update(thread_data->send_handle, NULL, &event_data);
 			}
+		
+			syslog(LOG_DEBUG,"GOT WORT TEMP OF : %f and AMBIENT OF %f and heatcool is %d", thread_data->temp_data.temp1,thread_data->temp_data.temp2,thread_data->heatcool);
+			if ( thread_data->temp_data.temp1 < (setpoint-0.3) ) {
+				syslog(LOG_NOTICE,"Heat Enabled");
+				if ( control_temp("1","0") ) {
+					thread_data->heatcool=2;
+				} else {
+					syslog(LOG_ERR,"Error, unable to enable heating");
+				}
+			}
+			if ( thread_data->temp_data.temp1 == setpoint || thread_data->temp_data.temp1 == (setpoint+0.1) || thread_data->temp_data.temp1 == (setpoint-0.1) ) {
+				syslog(LOG_NOTICE,"Heating and Cooling disabled");
+				if ( control_temp("0","0") ) {
+					thread_data->heatcool=1;
+				} else {
+					syslog(LOG_ERR,"Unable to disable heating and cooling\n");
+				}
+			}
+			if ( thread_data->temp_data.temp1 > (setpoint + 0.4)  && thread_data->temp_data.temp2 >= 3) {
+				syslog(LOG_NOTICE,"Chill Enabled\n");
+				if ( control_temp("0","1") ) {
+					thread_data->heatcool=3;
+				} else {
+					syslog(LOG_ERR,"Unable to enable the chiller\n");
+				}
+
+			}
+
+			sleep(5);
 		}
-		close(fp);
 		close(fp1);
-		printf("GOT WORT TEMP OF : %f and AMBIENT OF %f and heatcool is %d\n", thread_data->temp_data.temp1,thread_data->temp_data.temp2,thread_data->heatcool);
-		if ( thread_data->temp_data.temp1 < (setpoint-0.3) ) {
-			printf("TURNING ON HEATER\n");
-			system("echo 1 > /sys/class/gpio/gpio51/value");
-			system("echo 0 > /sys/class/gpio/gpio60/value");
-			thread_data->heatcool=2;
-		}
-		if ( thread_data->temp_data.temp1 == setpoint || thread_data->temp_data.temp1 == (setpoint+0.1) || thread_data->temp_data.temp1 == (setpoint-0.1) ) {
-			printf("TURNING OFF HEATING/COOLING!\n");
-			system("echo 0 > /sys/class/gpio/gpio51/value");
-			system("echo 0 > /sys/class/gpio/gpio60/value");
-			thread_data->heatcool=1;
-		}
-		if ( thread_data->temp_data.temp1 > (setpoint + 0.4)  && thread_data->temp_data.temp2 >= 3) {
-			printf("TURNING ON CHILLER\n");
-			system("echo 1 > /sys/class/gpio/gpio60/value");
-			system("echo 0 > /sys/class/gpio/gpio51/value");
-			thread_data->heatcool=3;
-			
-		}
-		sleep(5);
+		close(fp);
+	} else {
+		syslog(LOG_ERR, "unable to open temperature sensors");
 	}
 }
 
-int get_input(thdata *data) 
-{
-	char a[2];
-	char t[16];
-	while (1)
-	{
-		printf("Command >\n");
-		fgets(a,2,stdin);
-		if ( strcmp(a,"a") == 0 ) {
-			printf("\tAmbient temp is %f\n", data->temp_data.temp1);
-		}
-		if ( strcmp(a,"w") == 0 ) {
-				printf("\tWort Temp is %f\n", data->temp_data.temp2);
-		}
-		if ( strcmp(a,"t") == 0 ) {
-			printf("Input Temp as Float : ");
-			fgets(t,16,stdin);
-			printf("Setting setpoint to %f\n", atof(t));
-		    setpoint = atof(t);	
-		}
-		if ( strcmp(a,"s") == 0 ) {
-			printf("\t Setpoint is %f\n", setpoint);
-		}
-	}
-}
-enum lcfg_status example_visitor(const char *key, void *data, size_t len, void *user_data) {
+
+enum lcfg_status read_cfg(const char *key, void *data, size_t len, void *user_data) {
     int i;
     char c;
 
@@ -301,19 +271,27 @@ int main(int argc, char* argv[])
 	thdata data1;
 	size_t len;
 	struct lcfg *cfg = lcfg_new("./brew.cfg");
+	
+	//open the log
+	openlog("Fermtroller", LOG_PID, LOG_USER);
 
 	if ( lcfg_parse(cfg) != lcfg_status_ok ) {
-		printf("Error reading config file : %s\n", lcfg_error_get(cfg));
+		syslog(LOG_INFO,"Error reading config file : %s\n", lcfg_error_get(cfg));
 	} else {
-		lcfg_accept(cfg, example_visitor,0);
+		lcfg_accept(cfg, read_cfg,0);
 	}
 
 	lcfg_value_get(cfg, "wort_address",(void *)&temp_wort, &len);
 	lcfg_value_get(cfg, "ambient_address",(void *)&temp_ambient, &len);
 	if ( lcfg_value_get(cfg, "web_address", (void *)&web_address, &len) != lcfg_status_ok ) {
-		printf("Error reading web_address : %s\n", lcfg_error_get(cfg));
+		syslog(LOG_ERR,"Error reading web_address : %s\n", lcfg_error_get(cfg));
 	}
+
+	// assign the GPIO file descriptors
 	
+ 	cool_fd = open(cool_control,O_WRONLY);
+	heat_fd = open(heat_control,O_WRONLY);
+
 	data1.temp_data.temp1 = 0.0;
 	data1.temp_data.temp2 = 0.0;
 	data1.good_read = 0;	
@@ -337,18 +315,18 @@ int main(int argc, char* argv[])
 	}
 #ifdef STORYBOARD
 	if ( channel == NULL ) {
-		printf("You must supply a greio channel temp_driver -c <storyboard channel>\n");
+		syslog(LOG_ERR,"You must supply a greio channel temp_driver -c <storyboard channel>\n");
 		exit(-1);
 	}
 	send_handle = gre_io_open(channel, GRE_IO_TYPE_WRONLY);
 	if(send_handle == NULL && channel != NULL ) {
-		printf("Can't open send handle\n");
+		syslog(LOG_ERR,"Can't open send handle\n");
 		while ( not_connect == 0 ) {
 			send_handle = gre_io_open(channel, GRE_IO_TYPE_WRONLY);
 			if ( send_handle != NULL ) {
 				not_connect = 1;
 			}
-			printf("Waiting to connect\n");
+			syslog(LOG_INFO,"Waiting to connect\n");
 			sleep(1);
 		}
 	}
@@ -356,25 +334,25 @@ int main(int argc, char* argv[])
 	// push the send handle into the thread data pointer so we can access it in our check_tmep thread.
 	data1.send_handle = send_handle;
 #endif
-	printf("Starting temperature Driver\n");
+	syslog(LOG_INFO,"Fermtroller starting");
 	pthread_create(&temperature_thread, NULL, (void *)&check_temperature, &data1);
 	sleep(1);
 	pthread_create(&templog_thread, NULL, (void *)&log_temp, &data1);
 	ret = pthread_mutex_init(&mutex, NULL);
 	if(ret != 0) {
-		printf("Mutex error\n");
+		syslog(LOG_ERR,"Mutex error\n");
 		return -1;
 	}
 
 	ret = pthread_cond_init(&condvar, NULL);
 	if(ret != 0) {
-		printf("Condvar error\n");
+		syslog(LOG_ERR,"Condvar error\n");
 		return -1;
 	}
 	
-	get_input(&data1);
+	//get_input(&data1);
 	while(!done) {
-		printf("waiting\n");
+		syslog(LOG_INFO,"waiting\n");
 		pthread_mutex_lock(&mutex);
 		pthread_cond_wait(&condvar, &mutex);
 	}
@@ -384,8 +362,10 @@ int main(int argc, char* argv[])
 	free (temp_wort);
 	free (temp_ambient);
 	free (web_address);
-	printf("Done Temperature Driver\n");
+	syslog(LOG_INFO,"Fermtroller shutting down\n");
 
-
+	close(cool_fd);
+	close(heat_fd);
+	closelog();
 	return 0;
 }
